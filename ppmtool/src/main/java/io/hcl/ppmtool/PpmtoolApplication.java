bfc6335659109618a28e57495de8996896ff167b
Sreeja Paluri <sreeja.paluri@hcl.com>
2019-08-07 17:42:48 -0400

    first commit

--- ppmtool/src/main/java/io/hcl/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/io/hcl/ppmtool/PpmtoolApplication.java
@@ -1,4 +1,4 @@
-package io.agileintelligence.ppmtool;
+package io.hcl.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ppmtool/src/test/java/io/hcl/ppmtool/PpmtoolApplicationTests.java
+++ ppmtool/src/test/java/io/hcl/ppmtool/PpmtoolApplicationTests.java
@@ -1,4 +1,4 @@
-package io.agileintelligence.ppmtool;
+package io.hcl.ppmtool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
